63c2f6f3fe17dd08cebfcb6c586f77ebe60a5817
Vitalii Komisarenko <komisarenko.v@daikineurope.com>
2023-12-07 09:55:30 +0100

    Board: Move constructor code to constructors
    Move code constructing board to:
    - constructor (empty board of given size)
    - defaultBoard method for the starting position
    
    Fix fromString method

--- engine.js
+++ engine.js
@@ -40,19 +40,19 @@ class Tile {
 		@return a Tile object
 		*/
 		switch(str) {
-		case 'P': return Tile(PLAYER_WHITE, PIECE_PAWN)
-		case 'R': return Tile(PLAYER_WHITE, PIECE_ROOK)
-		case 'N': return Tile(PLAYER_WHITE, PIECE_KNIGHT)
-		case 'B': return Tile(PLAYER_WHITE, PIECE_BISHOP)
-		case 'Q': return Tile(PLAYER_WHITE, PIECE_QUEEN)
-		case 'K': return Tile(PLAYER_WHITE, PIECE_KING)
-		case 'p': return Tile(PLAYER_BLACK, PIECE_PAWN)
-		case 'r': return Tile(PLAYER_BLACK, PIECE_ROOK)
-		case 'n': return Tile(PLAYER_BLACK, PIECE_KNIGHT)
-		case 'b': return Tile(PLAYER_BLACK, PIECE_BISHOP)
-		case 'q': return Tile(PLAYER_BLACK, PIECE_QUEEN)
-		case 'k': return Tile(PLAYER_BLACK, PIECE_KING)
-		case '.': return Tile(PLAYER_NONE, PIECE_NONE)
+		case 'P': return new Tile(PLAYER_WHITE, PIECE_PAWN)
+		case 'R': return new Tile(PLAYER_WHITE, PIECE_ROOK)
+		case 'N': return new Tile(PLAYER_WHITE, PIECE_KNIGHT)
+		case 'B': return new Tile(PLAYER_WHITE, PIECE_BISHOP)
+		case 'Q': return new Tile(PLAYER_WHITE, PIECE_QUEEN)
+		case 'K': return new Tile(PLAYER_WHITE, PIECE_KING)
+		case 'p': return new Tile(PLAYER_BLACK, PIECE_PAWN)
+		case 'r': return new Tile(PLAYER_BLACK, PIECE_ROOK)
+		case 'n': return new Tile(PLAYER_BLACK, PIECE_KNIGHT)
+		case 'b': return new Tile(PLAYER_BLACK, PIECE_BISHOP)
+		case 'q': return new Tile(PLAYER_BLACK, PIECE_QUEEN)
+		case 'k': return new Tile(PLAYER_BLACK, PIECE_KING)
+		case '.': return new Tile(PLAYER_NONE, PIECE_NONE)
 		default: throw Error('Cannot create Tile from string: ' + str)
 		}
 	}
@@ -88,9 +88,51 @@ Tile.prototype.toString = function tile_to_string() {
 
 class Board {
 	constructor(board_width, board_height) {
-		this.tiles = make_board()
+		/**
+		 Create an empty board of given width and height
+		 */
 		this.width = board_width
 		this.height = board_height
+
+		this.tiles = []
+		for (let i = 0; i < board_width; ++i) {
+			let empty_file = []
+			for (let j = 0; j < board_height; ++j) {
+				empty_file.push(new Tile(PLAYER_NONE, PIECE_NONE))
+			}
+			this.tiles.push(empty_file)
+		}
+	}
+
+	static defaultBoard() {
+		/**
+		 * Create an ordinary chess board
+		 */
+		board = new Board(DEFAULT_BOARD_WIDTH, DEFAULT_BOARD_HEIGHT)
+
+		board.tiles[0][0] = new Tile(PLAYER_WHITE, PIECE_ROOK)
+		board.tiles[0][1] = new Tile(PLAYER_WHITE, PIECE_KNIGHT)
+		board.tiles[0][2] = new Tile(PLAYER_WHITE, PIECE_BISHOP)
+		board.tiles[0][3] = new Tile(PLAYER_WHITE, PIECE_QUEEN)
+		board.tiles[0][4] = new Tile(PLAYER_WHITE, PIECE_KING)
+		board.tiles[0][5] = new Tile(PLAYER_WHITE, PIECE_BISHOP)
+		board.tiles[0][6] = new Tile(PLAYER_WHITE, PIECE_KNIGHT)
+		board.tiles[0][7] = new Tile(PLAYER_WHITE, PIECE_ROOK)
+		board.tiles[7][0] = new Tile(PLAYER_BLACK, PIECE_ROOK)
+		board.tiles[7][1] = new Tile(PLAYER_BLACK, PIECE_KNIGHT)
+		board.tiles[7][2] = new Tile(PLAYER_BLACK, PIECE_BISHOP)
+		board.tiles[7][3] = new Tile(PLAYER_BLACK, PIECE_QUEEN)
+		board.tiles[7][4] = new Tile(PLAYER_BLACK, PIECE_KING)
+		board.tiles[7][5] = new Tile(PLAYER_BLACK, PIECE_BISHOP)
+		board.tiles[7][6] = new Tile(PLAYER_BLACK, PIECE_KNIGHT)
+		board.tiles[7][7] = new Tile(PLAYER_BLACK, PIECE_ROOK)
+
+		for (let i = 0; i < DEFAULT_BOARD_WIDTH; ++i) {
+			board.tiles[1][i] = new Tile(PLAYER_WHITE, PIECE_PAWN)
+			board.tiles[6][i] = new Tile(PLAYER_BLACK, PIECE_PAWN)
+		}
+
+		return board
 	}
 
 	static fromString(str, board_width = DEFAULT_BOARD_WIDTH, board_height = DEFAULT_BOARD_HEIGHT) {
@@ -110,10 +152,10 @@ class Board {
 		str = str.replace(/\s/g, '');
 
 		board = new Board(board_width, board_height)
-		for (let i = 0; i < str; ++i) {
-			rank_from_top = Math.floor(i % board_width)
-			rank = board_height - rank_from_top - 1
-			file = i % board_height
+		for (let i = 0; i < str.length; ++i) {
+			let rank_from_top = Math.floor(i / board_width)
+			let rank = board_height - rank_from_top - 1
+			let file = i % board_height
 			board.tiles[rank][file] = Tile.fromString(str.charAt(i))
 		}
 
@@ -261,43 +303,6 @@ class Game {
 		this.white_player_info = PlayerInfo()
 		this.black_player_info = PlayerInfo()
 	}
-}
-
-function make_empty_board(board_width, board_height) {
-	res = []
-	for (let i = 0; i < board_width; ++i) {
-		empty_file = []
-		for (let j = 0; j < board_height; ++j) {
-			empty_file.push(new Tile(PLAYER_NONE, PIECE_NONE))
-		}
-		res.push(empty_file)
-	}
-	return res
-}
-
-function make_board() {
-	res = make_empty_board(DEFAULT_BOARD_WIDTH, DEFAULT_BOARD_HEIGHT)
-	res[0][0] = new Tile(PLAYER_WHITE, PIECE_ROOK)
-	res[0][1] = new Tile(PLAYER_WHITE, PIECE_KNIGHT)
-	res[0][2] = new Tile(PLAYER_WHITE, PIECE_BISHOP)
-	res[0][3] = new Tile(PLAYER_WHITE, PIECE_QUEEN)
-	res[0][4] = new Tile(PLAYER_WHITE, PIECE_KING)
-	res[0][5] = new Tile(PLAYER_WHITE, PIECE_BISHOP)
-	res[0][6] = new Tile(PLAYER_WHITE, PIECE_KNIGHT)
-	res[0][7] = new Tile(PLAYER_WHITE, PIECE_ROOK)
-	res[7][0] = new Tile(PLAYER_BLACK, PIECE_ROOK)
-	res[7][1] = new Tile(PLAYER_BLACK, PIECE_KNIGHT)
-	res[7][2] = new Tile(PLAYER_BLACK, PIECE_BISHOP)
-	res[7][3] = new Tile(PLAYER_BLACK, PIECE_QUEEN)
-	res[7][4] = new Tile(PLAYER_BLACK, PIECE_KING)
-	res[7][5] = new Tile(PLAYER_BLACK, PIECE_BISHOP)
-	res[7][6] = new Tile(PLAYER_BLACK, PIECE_KNIGHT)
-	res[7][7] = new Tile(PLAYER_BLACK, PIECE_ROOK)
-	for (let i = 0; i < DEFAULT_BOARD_WIDTH; ++i) {
-		res[1][i] = new Tile(PLAYER_WHITE, PIECE_PAWN)
-		res[6][i] = new Tile(PLAYER_BLACK, PIECE_PAWN)
-	}
-	return res
 }
 
 function run_all_tests() {
